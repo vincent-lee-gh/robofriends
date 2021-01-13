5c2558fcb7828b4f8954b509d7be51ff45da522c
Vincent-Lee <vincentflee@outlook.com>
2021-01-13 16:49:30 +1100

    added No-Result feature

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,24 +3,30 @@ import  Card  from './Card';
 // import { robots } from "../robo";
 
 const CardList = ({robots}) =>{
-      return(
-        <div>
-            {
-              robots.map((user,i) =>{
-                return (
-                  <Card 
-                    key={i} 
-                    pokeName={robots[i].id} 
-                    name={robots[i].name} 
-                    email={robots[i].email} 
-                  />
-                );
-              })
-            }
-        </div>
-      );
-    
-  
+  if (robots.length <= 0){
+    return(
+      <div className='noResult'>
+        <h1>&#129302;Ummm, no result, plz try other keywords.&#129302;</h1>
+      </div>
+    )}else{
+    return(
+      <div>
+          {
+            robots.map((user,i) =>{
+              return (
+                <Card 
+                  key={i} 
+                  pokeName={robots[i].id} 
+                  name={robots[i].name} 
+                  email={robots[i].email} 
+                />
+              );
+            })
+          }
+      </div>
+    );
+  }
+
 }
 
 export default CardList;
